90a93b071ce63762daf1957e73c666d12364d7a0
sarveshdakhore <sarveshd444@gmail.com>
2024-03-15 01:28:10 +0530

    structure done

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from getpass import getpass
 # Get the personal access token from the user
 token = ''
 
-def get_repo_structure(repo_url, path=''):
+def get_repo_structure_clean(repo_url, path=''):
     # Extract the owner and repo name from the URL
     owner, repo = repo_url.split('github.com/')[-1].split('/')
     
@@ -19,15 +19,15 @@ def get_repo_structure(repo_url, path=''):
     headers = {'Authorization': f'token {token}'}
     response = requests.get(api_url, headers=headers)
     
+    
     # If the request was successful, process the JSON response
     if response.status_code == 200:
         contents = response.json()
         structure = []
-        
         for item in contents:
             if item['type'] == 'dir':
                 # If the item is a directory, fetch its contents recursively
-                item_structure = get_repo_structure(repo_url, item['path'])
+                item_structure = get_repo_structure_clean(repo_url, item['path'])
                 
                 structure.append({
                     'name': item['name'],
@@ -40,17 +40,42 @@ def get_repo_structure(repo_url, path=''):
                     'name': item['name'],
                     'type': 'file'
                 })
-        
         return structure
     else:
         return None
+    
 
-@app.route('/get_structure', methods=['POST'])
-def get_structure():
+def get_repo_structure_blob(repo_url, path=''):
+    # Extract the owner and repo name from the URL
+    owner, repo = repo_url.split('github.com/')[-1].split('/')
+    
+    # GitHub API URL for the repo contents
+    api_url = f'https://api.github.com/repos/{owner}/{repo}/contents/{path}'
+    
+    # Send a GET request to the GitHub API
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(api_url, headers=headers)
+    contents = response.json()
+    if response.status_code == 200:
+        return contents
+    else:
+        return None
+
+@app.route('/get_structure_clean', methods=['POST'])
+def get_structure_clean():
     data = request.get_json()
     repo_url = data['key']
-    structure = get_repo_structure(repo_url)
-    return jsonify(structure)
+    structure = get_repo_structure_clean(repo_url)
+    return {'clean': structure}, 200
+
+@app.route('/get_structure_blob', methods=['POST'])
+def get_structure_blob():
+    data = request.get_json()
+    repo_url = data['key']
+    blob = get_repo_structure_blob(repo_url)
+    return {'blob':blob}, 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
