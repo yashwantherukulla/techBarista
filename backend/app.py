c3deff41d5a3aa4fe2fff4fe7fb528bbb35deace
SamShah30112004 <harshdeepashah@gmail.com>
2024-03-15 04:40:34 +0530

    Removed key

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)  
 
 import base64
-token = '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'
+token = ''
 def get_repo_structure_comb(repo_url, path=''):
     # Extract the owner and repo name from the URL
     owner, repo = repo_url.split('github.com/')[-1].split('/')
